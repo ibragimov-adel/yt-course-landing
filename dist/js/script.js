79e54856d7ec8a320cac81eec6a23a1afe4e5cd6
Adel Ibragimov <ibragimov.adel.kzn@gmail.com>
2021-11-13 12:04:55 +0300

    add block with modules

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+const ACTIVE_MODULE_CONTENT_CLASS = 'modules__content-wrapper_active';
+const ACTIVE_MODULE_CONTROL_CLASS = 'modules__nav-item_active';
 
+document.addEventListener('DOMContentLoaded', () => {
+	let currentModule = '01';
+	const modulesControls = document.querySelectorAll('.modules__nav-item');
+	const modulesContents = document.querySelectorAll('.modules__content-wrapper');
+
+	modulesControls.forEach(elem => elem.addEventListener('click', () => {
+		const targetID = elem.id.split('-')[2];
+
+		const currentContent = document.querySelector(`#module-content-${currentModule}`);
+		const targetContent = document.querySelector(`#module-content-${targetID}`);
+		const currentControl = document.querySelector(`#module-nav-${currentModule}`);
+		const targetControl = document.querySelector(`#module-nav-${targetID}`);
+
+		currentContent.classList.remove(ACTIVE_MODULE_CONTENT_CLASS);
+		currentControl.classList.remove(ACTIVE_MODULE_CONTROL_CLASS);
+		targetContent.classList.add(ACTIVE_MODULE_CONTENT_CLASS);
+		targetControl.classList.add(ACTIVE_MODULE_CONTROL_CLASS);
+
+		currentModule = targetID;
+	}));
 });
