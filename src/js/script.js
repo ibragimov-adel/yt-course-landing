fa0119bff09396dcd7817402d7aad940c50bbd36
Adel Ibragimov <ibragimov.adel.kzn@gmail.com>
2021-11-14 10:59:56 +0300

    finished all blocks

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const ACTIVE_MODULE_CONTENT_CLASS = 'modules__content-wrapper_active';
 const ACTIVE_MODULE_CONTROL_CLASS = 'modules__nav-item_active';
 
+const ACTIVE_MODAL_OVERLAY_CLASS = 'modals__overlay_opened';
+const ACTIVE_MODAL_CLASS = 'modal_opened';
+
 document.addEventListener('DOMContentLoaded', () => {
 	let currentModule = '01';
 	const modulesControls = document.querySelectorAll('.modules__nav-item');
@@ -21,4 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		currentModule = targetID;
 	}));
+
+	const modalOverlay = document.querySelector('.modals__overlay');
+	let activeModal;
+
+	document.querySelectorAll('[data-control=modal]').forEach((elem) => {
+		const targetID = elem.dataset.target;
+
+		elem.addEventListener('click', () => {
+			activeModal = document.querySelector(`#${targetID}`);
+
+			if (elem.dataset.video) {
+				const iframe = activeModal.querySelector('iframe');
+				iframe.src = 'https://youtube.com/embed/' + elem.dataset.video;
+			}
+
+			modalOverlay.classList.add(ACTIVE_MODAL_OVERLAY_CLASS);
+			activeModal.classList.add(ACTIVE_MODAL_CLASS);
+		});
+	});
+
+	modalOverlay.addEventListener('click', () => {
+		modalOverlay.classList.remove(ACTIVE_MODAL_OVERLAY_CLASS);
+		activeModal.classList.remove(ACTIVE_MODAL_CLASS);
+	});
+
+	document.querySelectorAll('.modal').forEach(elem => {
+		elem.addEventListener('click', e => e.stopPropagation());
+	});
 });
